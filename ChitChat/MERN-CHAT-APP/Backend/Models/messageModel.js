e3ae03d16b8bc2a871b1b3ed1c9a810a49da8740
HIMANSHI DAYAL <120187837+himanshidayal14@users.noreply.github.com>
2024-01-29 02:50:45 +0530

    final commited

---
+++ ChitChat/MERN-CHAT-APP/Backend/Models/messageModel.js
@@ -0,0 +1,17 @@
+const mongoose = reqquire("mongoose");
+
+const messageModel = mongoose.Schema(
+    {
+        sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        content: { type: String, trim: true },
+        chat: { type: mongoose.Schema.Type.ObjectId, ref: "Chat" },
+        
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Message = mongoose.model("Message", messageModel);
+
+module.exports = Message;
